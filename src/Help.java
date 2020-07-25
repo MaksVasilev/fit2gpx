24ff6af859addd118a59305a819c6c6689a2c348
MaksVasilev <max@stranger-team.ru>
2020-07-25 18:20:39 +0300

    Help localization

--- src/Help.java
+++ src/Help.java
@@ -9,35 +9,26 @@ http://velo100.ru/garmin-fit-to-gpx
 
 class Help {
 
-    private static final String Version = "\nКонвертер Garmin .FIT в .GPX/.CVS, Версия 0.0.4";
+    private static final String Version = "\n" + fit2gpx.tr.getString("Help_Version") ;
 
     static void usage() {
-        System.out.println(Version + "\n\n© Maks Vasilev, 2015-2020, http://velo100.ru/garmin-fit-to-gpx\nFIT Software Development Kit (SDK), http://www.thisisant.com/resources/fit\n");
-        System.out.println("Информация по использованию:\n\tjava -jar fit2gpx.jar --help\n");
-        System.out.println("Консольный режим:\n\tjava -jar fit2gpx.jar [--statistic|-s] <файл Garmin .FIT> [[<файл Garmin .FIT>] …]\n");
-        System.out.println("Графический диалоговый режим:\n\tjava -jar fit2gpx.jar [--statistic|-s]\n");
-        System.out.println("\t\t--statistic\t| -s\tвывод итоговой статистики в консоль");
-        System.out.println("\t\t--csv\t\t| -c\tвыходной файл будет в формате CSV");
-        System.out.println("\t\t--hr-only\t\tвыходной файл будет в формате CSV, содержимое: только ЧСС и время");
-        System.out.println("\t\t--monitor\t| -m\tразбор файлов мониторинга пульса (не тренировки), выходной файл будет в формате CSV");
-        System.out.println("\t\t--hrv\t\t| -v\tразбор файлов тренировки и запись интервалов R-R для анализа вариабельности, выходной файл будет в формате CSV");
-        System.out.println("\nКонвертер имеет два режима работы: консольный и графический. При запуске без параметров запускается диалоговое окно");
-        System.out.println("выбора файла для конвертации. Если при запуске в качестве аргумента передать имя файла, то происходит конвертация");
-        System.out.println("без запуска диалоговых окон. В случае успешной конвертации никакой информации в консоль не возвращается.");
-        System.out.println("Консольный режим работы можно использовать в различных скриптах для автоматизации.");
-        System.out.println("\nКонвертер поддерживает режим пакетной обработки файлов, для этого в консольном режиме укажите имена всех файлов");
-        System.out.println("в качестве аргументов программы. В диалоговом режиме просто выберите несколько файлов в диалоге выбора.");
-        System.out.println("\nДамп всех заголовков файла (отладочный режим):");
-        System.out.println("\tjava -cp fit2gpx.jar DebugDecode <файл Garmin .FIT>\n");
+        System.out.println(Version + "\n\n" + fit2gpx.tr.getString("Help_Author"));
+        System.out.println(fit2gpx.tr.getString("Help_line01"));
+        System.out.println(fit2gpx.tr.getString("Help_line02"));
+        System.out.println(fit2gpx.tr.getString("Help_line03"));
+        System.out.println(fit2gpx.tr.getString("Help_line04"));
+
+        System.out.println(fit2gpx.tr.getString("Help_line09"));
+        System.out.println(fit2gpx.tr.getString("Help_line10"));
+        System.out.println(fit2gpx.tr.getString("Help_line11"));
 
         System.exit(64);
     }
 
     static void reader_usage() {
-        System.out.println(Version + "\n\n© Maks Vasilev, 2015-2020, http://velo100.ru/garmin-fit-to-gpx\nFIT Software Development Kit (SDK), http://www.thisisant.com/resources/fit\n");
+        System.out.println(Version + "\n\n" + fit2gpx.tr.getString("Help_Author"));
         System.out.println("Информация по использованию:\n\tjava -jar FitReader.jar <файл Garmin .FIT> > <выходной файл>\n");
         System.exit(64);
     }
 
 }
-
